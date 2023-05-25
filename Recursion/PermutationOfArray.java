32df9c8a8dc79735445b23851bdddc66ba1656e8
Manik Gupta <guptamanik1810@gmail.com>
2023-05-25 13:35:03 +0530

    Update PermutationOfArray.java

--- Recursion/PermutationOfArray.java
+++ Recursion/PermutationOfArray.java
@@ -24,3 +24,44 @@ class Solution {
         return ans;
     }
 }
+
+
+
+
+
+
+
+
+//Method 2  swapping
+
+
+class Solution {
+
+    static void swap(int i,int j,int []nums){
+        int t=nums[i];
+        nums[i]=nums[j];
+        nums[j]=t;
+    }
+
+    static void solve(int index,List<List<Integer>> ans,int []nums){
+        if(index==nums.length){
+            List<Integer> al = new ArrayList<>();
+            for(int x:nums){
+                al.add(x);
+            }
+            ans.add(new ArrayList<>(al));
+            return;
+        }
+        for(int i=index;i<nums.length;i++){
+            swap(i,index,nums);
+            solve(index+1,ans,nums);
+            swap(i,index,nums);
+        }
+    }
+
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans= new ArrayList<>();
+        solve(0,ans,nums);
+        return ans;
+    }
+}
